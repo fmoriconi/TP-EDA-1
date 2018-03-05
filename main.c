87a702b5ea3c0168c72cda4b40524557d57a7920
Francisco Musich <fmusich@itba.edu.ar>
2018-03-05 16:43:33 -0300

    hola

--- main.c
+++ main.c
@@ -3,18 +3,45 @@
 #define PROGRAMNAME "parseprog"
 #define NUM_TESTS 9
 #define MAX_TEST 10
+#define CANTIDAD_OPCIONES 3
+#define CANTIDAD_VALORES 3
+#define CANTIDAD_PARAMETROS 3
+#define TAMAÑO_VALORES 15
 
 int parseCallback(char *key, char *value, void *userData); //Devuelve 1 si la interpretación es correcta y 0 si no lo es.
 
-// test
+
 
 //Parametros takehome, forhere, napkin
-//Opciones food: pizza, burger, pasta // beverage: coke, water // size: small, medium, big
+//Opciones food: pizza, burger, pasta // beverage: coke, water,sprite // size: small, medium, big
+
+/**********************
+*	Estructura Option
+	Consiste en un arreglo de char en el cual entrara la clave.
+	Luego un arreglo de punteros a char, que estos seran los posibles valores para esta clave.
+	Estos valores deben asignarse antes de compilar
+***********************/
+typedef struct {
+	char clave[10];
+	char * valores[CANTIDAD_VALORES]; //En caso de que haya opciones con cantidad de valores distinta, llenarla con algo invalido asi la copmaracion no se cumple
+} option_t;
+/*********************
+*	Estructura userdata
+	Contiene un arreglo de option_t dentro de donde iran todas las posibles opciones (con sus claves y valores correspondientes).
+	Tambien contiene un arreglo de punteros a char, que seran los punteros a los strings para los parametros.
+**********************/
+typedef struct {
+	option_t opciones[CANTIDAD_OPCIONES];
+	
+	char *parametros[CANTIDAD_PARAMETROS]; // Los creo como punteros a char, ya que asumo que los strings se van a guardar en ROM
+
+} userdata_t;
 
 int main (void)
 {
 	// Testing Bench
 	// 
+	userdata_t valEstablecidos = { {"food"} {"pizza"} {"burguer"} {"pasta"} {"beverage"} {"coke"} {"water"} {"sprite"} {"size"} {"small"} {"medium"} {"big"} {"takehome"} {"forhere"} {"napkin"} }
 	char *attempts[NUM_TESTS][]= {{PROGRAMNAME, "-clothes", "trouser" , NULL } // unexist option and value
 						 {PROGRAMNAME, "shop", NULL } // unexist parameter
 						 {PROGRAMNAME, "-food", NULL } // non-value for the option
@@ -26,6 +53,7 @@ int main (void)
 						 {PROGRAMNAME, "-food", "pizza", "-beverage", "water", "forhere", NULL } // good try
 						};
 	int i;
+	int quantity;
     for (i = 0; i < NUM_TESTS; i++)
 	{
 		quantity = parseCmdLine(,,,);
@@ -43,19 +71,58 @@ int main (void)
 	getchar();
 	return 0;
 }
-
+/***********************************
+	Funcion parseCallback
+************************************/
+/*  Esta funcion recibe 3 parametros, key y value corrsponden a los strings a analizar,
+	y userData es un pasaje por referencia de una estructura en la cual se encuentran las posibles opciones y parametros validos.
+	La funcion trabaja comparando los strings recibidos con aquellos encontrados en la estructura.
+*/
 int parseCallback(char *key, char *value, void *userData) {
 
-	bool validargument;
-	userdata * data = (userdata*)userData; //Esto es medio redundante, lo sé, pero visual studio no me dejaba desreferenciarlo bien si no lo hacía.
-
-	if(key != NULL){
-
+	userdata_t * data = (userdata_t*)userData; //Esto es medio redundante, lo sé, pero visual studio no me dejaba desreferenciarlo bien si no lo hacía. (creo que esta bien por que hay que castear el puntero ese que es void, hay que decirle que es la estructura que creamos)
+	int validez; //Respuesta del callback
+	if (key == NULL) // Analizo parametros
+	{
+		int i;
+		int cerrarBucle = 0;
+		for (i = 0; (i < CANTIDAD_PARAMETROS)&&(cerrarBucle == 0); i++) //Ciclo el arreglo de parametros para buscar cual coincide
+		{
+			if(!(strcmp(strupr(value), strupr(data->parametros[i])))) // analizo si el string en el arreglo de parametros es igual al enviado por el parser
+			{
+				cerrarBucle = 1;  // Paro el ciclado
+				validez = 1; //responde que los parametros son validos
+			}
+			else  //ocurre en el caso que se mande mal un parametro y no se encuentre en aquellos especificados por el usuario
+			{
+				validez = 0;
+			}
+		}
+	}
+	else 
+	{
+		int i, j;
+		int cerrarBucle;
+		for (i = 0; (i < CANTIDAD_OPCIONES) && (cerrarBucle == 0); i++) //ciclo el arreglo de opciones hasta hayar la clave igual
+		{
+			if (!(strcmp(strupr(key), strupr(data->opciones[i].clave))))
+			{
+				for (j = 0; (j < CANTIDAD_VALORES) && (cerrarBucle == 0);j++)// cuando la encuentro analizo posibles valores dentro de esa opcion, en el arreglo de valores
+				{
+					if (!strcmp(strupr(value), strupr(data->opciones[i].valores[j]))) //si se encuentra un valor valido entonces se cierra el loop y se responde un 1
+					{
+						    validez =1;
+							cerrarBucle =1;
+					}
+				}
+			}
+			else  //ocurre en el caso en el que la clave no es valida, o si la clave es valida, pero no su valor
+			{
+				validez = 0;
+			}
+		}
+		
 	}
 
-	else {
-
-	}
-
-	return validargument;
+	return validez;
 }
